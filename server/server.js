ecf66e8c0cb65b0a995b2eef7f2381d3f8f98614
Priyadubal18 <priya.dubal18@gmai.com>
2019-06-18 21:44:05 -0400

    Completed the proxy server

--- server/server.js
+++ server/server.js
@@ -7,10 +7,11 @@ app.use(cors());
 
 app.use('/:id', express.static('client'));
 app.use('/hundred/:id', proxy({ target: 'http://localhost:4554', changeOrigin: true }));
-app.use('/productInfo', proxy({ target: 'http://localhost:4000', changeOrigin: true }));
+app.use('/productInfo/:id', proxy({ target: 'http://localhost:4000', changeOrigin: true }));
 app.use('/productBuyerService/:id', proxy({ target: 'http://localhost:4554', changeOrigin: true }));
 app.use('/productQtyInfo', proxy({ target: 'http://localhost:4000', changeOrigin: true }));
 app.use('/Priya/:id', proxy({ target: 'http://localhost:3004', changeOrigin: true }));
+app.use('/review/:id', proxy({ target: 'http://localhost:3004', changeOrigin: true }));
 
 
 app.listen(port, () => console.log(`NordStorm is running on port 127.0.0.1:${port}`));
