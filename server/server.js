470360174d8bc4151dbb3d3352c05550963b68b3
Priyadubal18 <priya.dubal18@gmai.com>
2019-06-28 22:50:10 -0400

    Completed the process

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var proxy = require('http-proxy-middleware');
 var cors = require('cors');
 app.use(cors());
 
+console.log("pp");
+
 app.use('/:id', express.static('client'));
 app.use('/hundred/:id', proxy({ target: 'http://ec2-54-175-0-74.compute-1.amazonaws.com', changeOrigin: true }));
 app.use('/productInfo/:id', proxy({ target: 'http://ec2-3-86-76-90.compute-1.amazonaws.com', changeOrigin: true }));
